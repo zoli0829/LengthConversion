e2e93bde616e86f637f22b9718d9153ca616d8dd
zoli0829 <zoli0829@gmail.com>
2025-04-05 11:17:55 +0100

    Initial commit

---
+++ LengthConversion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LengthConversion
+//
+//  Created by Zoltan Vegh on 05/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LengthConversion/LengthConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LengthConversionApp.swift
+//  LengthConversion
+//
+//  Created by Zoltan Vegh on 05/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LengthConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
